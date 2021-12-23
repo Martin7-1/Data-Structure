45a314b879bea894c28cdadfba5acdc345c00a3a
Martin77 <1754350460@qq.com>
2021-12-23 10:41:45 +0800

    update make tree method

--- data-structure/src/main/java/com/nju/edu/tree/BinaryTree.java
+++ data-structure/src/main/java/com/nju/edu/tree/BinaryTree.java
@@ -1,13 +1,34 @@
 package com.nju.edu.tree;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class BinaryTree<T extends Comparable<? extends T>> {
     BinaryNode<T> root;
 
     public BinaryTree(BinaryNode<T> root) {
         this.root = root;
+    }
+
+    public BinaryTree() {
+        this(null);
+    }
+
+    public BinaryTree(String pre, String in) {
+        // 用先序遍历和中序遍历来构造一棵树
+        makeTree(pre, in);
+    }
+
+    /**
+     * 根据中序遍历和先序遍历构建一棵二叉树
+     * @param pre 先序遍历的节点顺序
+     * @param in 中序遍历的节点顺序
+     */
+    private void makeTree(String pre, String in) {
+        // TODO: 根据先序遍历和中序遍历构建一棵树   
     }
 
     /**
@@ -78,7 +99,28 @@ public class BinaryTree<T extends Comparable<? extends T>> {
      * @return 层级遍历的结果
      */
     public List<T> levelorderTraverse() {
-        return null;
+        // 使用一个队列
+        Deque<BinaryNode<T>> queue = new LinkedList<>();
+        List<T> list = new ArrayList<>();
+        BinaryNode<T> node = root;
+
+        while (root != null) {
+            list.add(node.element);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+
+            try {
+                node = queue.getFirst();
+            } catch (NoSuchElementException e) {
+                break;
+            }
+        }
+
+        return list;
     }
     
 }
---
+++ data-structure/src/test/java/com/nju/edu/tree/TraverseTest.java
@@ -0,0 +1,21 @@
+package com.nju.edu.tree;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TraverseTest {
+
+    private static BinaryTree<Integer> tree;
+    private int[] expect;
+    private int[] actual;
+    
+    @BeforeClass
+    public static void setUp() {
+        tree = new BinaryTree<>();
+    }
+
+    @Test
+    public void inorderTest1() {
+        
+    }
+}
