3d24d67f0affe0b584c95c3bfe2789475306c502
Martin77 <1754350460@qq.com>
2021-12-24 10:29:53 +0800

    update maketree

--- data-structure/src/main/java/com/nju/edu/tree/MakeTree.java
+++ data-structure/src/main/java/com/nju/edu/tree/MakeTree.java
@@ -1,10 +1,14 @@
 package com.nju.edu.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeTree {
 
     BinaryNode<Character> root;
 
     public MakeTree(String pre, String in) {
+        root = new BinaryNode<>();
         makeTree(pre, in, root);
     }
 
@@ -17,7 +21,8 @@ public class MakeTree {
             node = null;
             return;
         } else {
-            node = new BinaryNode<>(pre.charAt(0));
+            // 必须先让root指向一个对象，否则传进来的引用新建对象是无法被root所指到的
+            node.element = pre.charAt(0);
             // 找到中序遍历里的根节点
             while (in.charAt(inPos) != pre.charAt(0)) {
                 inPos++;
@@ -25,14 +30,47 @@ public class MakeTree {
             // 左子树
             preTemp = pre.substring(1, inPos + 1);
             inTemp = in.substring(0, inPos);
+            if (preTemp.length() != 0) {
+                node.left = new BinaryNode<>();
+            }
             makeTree(preTemp, inTemp, node.left);
 
             // 右子树
             preTemp = pre.substring(inPos + 1);
             // 注意这里中序遍历要跳过根节点
             inTemp = in.substring(inPos + 1);
+            if (preTemp.length() != 0) {
+                node.right = new BinaryNode<>();
+            }
             makeTree(preTemp, inTemp, node.right);
         }
     }
-    
+
+    /**
+     * 后序遍历，测试方法
+     * 
+     * @return
+     */
+    public List<Character> postorderTraverse() {
+        List<Character> ans = new ArrayList<>();
+        postorderTraverse(root, ans);
+        return ans;
+    }
+
+    /**
+     * 后序遍历的递归方法
+     * 
+     * @param node 当前遍历的节点
+     * @param list 后序遍历存储的list
+     */
+    private void postorderTraverse(BinaryNode<Character> node, List<Character> list) {
+        if (node == null) {
+            return;
+        }
+
+        postorderTraverse(node.left, list);
+        postorderTraverse(node.right, list);
+        list.add(node.element);
+    }
+
 }
--- data-structure/src/test/java/com/nju/edu/tree/MakeTreeTest.java
+++ data-structure/src/test/java/com/nju/edu/tree/MakeTreeTest.java
@@ -1,12 +1,37 @@
 package com.nju.edu.tree;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
+/**
+ * 根据前序遍历和中序遍历来生成一棵二叉树
+ * @author Martin
+ */
 public class MakeTreeTest {
+
+    String pre;
+    String in;
+    MakeTree tree;
+    String expect;
+    String actual;
 
     @Test
     public void makeTreeTest1() {
-        
+        pre = "ABDCEGFHI";
+        in = "DBAEGCHFI";
+        tree = new MakeTree(pre, in);
+
+        // 根据后序遍历来比对
+        // 转换一个character的list到string
+        List<Character> chars = tree.postorderTraverse();
+        actual = chars.stream().map(String::valueOf).collect(Collectors.joining());
+        expect = "DBGEHIFCA";
+
+        assertEquals(expect, actual);
     }
     
 }
